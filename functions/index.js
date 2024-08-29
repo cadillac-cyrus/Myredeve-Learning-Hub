d8506c0da74642b5a3b877f4977154704e7db40f
HexaCodeLabs <Hexacodelabs@gmail.com>
2024-08-29 17:23:34 +0000

    commit

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,48 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const emailjs = require('emailjs');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Initialize Firebase Admin SDK
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// EmailJS server setup
+const server = emailjs.server.connect({
+    user: "Coxwell",
+    password: "G7$k@2Xp",
+    host: "smtp.gmail.com",  // Use your SMTP provider details
+    ssl: true,
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendEmailOnNewFormSubmission = functions.firestore
+    .document('submissions/{docId}')
+    .onCreate((snap, context) => {
+        const newValue = snap.data();
+        const { name, email, subject, message } = newValue;
+
+        // Set up your template parameters here
+        const templateParams = {
+            name: name,
+            email: email,
+            subject: subject,
+            message: message,
+        };
+
+        // Send the email using EmailJS with the template ID
+        return emailjs.send({
+            user: "Coxwell",
+            password: "G7$k@2Xp",
+            host: "smtp.gmail.com",
+            ssl: true,
+            template: 'template_ym0bbq7', // Your EmailJS template ID
+            templateParams: templateParams,
+            from: "Your Name <hexacodelabs@gmail.com>",
+            to: "Recipient Name <myredevesuper@gmail.com>",
+            subject: `New Form Submission: ${subject}`,
+        }, (err, message) => {
+            if (err) {
+                console.error('Error sending email:', err);
+            } else {
+                console.log('Email sent successfully using template!', message);
+            }
+        });
+    });
